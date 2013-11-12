e9134acaa7e354f7a5fb66972165ec66b021463f
MrExpendable <christianr465@gmail.com>
2013-11-12 14:19:32 -0700

    Matches the file correctly now

--- src/chess/IO/FileIO.java
+++ src/chess/IO/FileIO.java
@@ -21,12 +21,86 @@ public class FileIO
 			while(bReader.ready())
 			{
 				nextLine = bReader.readLine();
-				Pattern newPattern = Pattern.compile("([BKNPQR])([dl])([a-h][1-8])");
+				Pattern newPattern = Pattern.compile("([BKNPQR][dl])([a-h][1-8])");
 				Matcher matcher = newPattern.matcher(nextLine);
 				
 				while(matcher.find())
 				{
-					System.out.printf("Piece: %s\nColor: %s\nPosition: %s%n", matcher.group(1), matcher.group(2), matcher.group(3));
+					String pieceType = matcher.group(1);
+					String position = matcher.group(2);
+					
+					//Bishop
+					if(pieceType.contains("B"))
+					{
+						if(pieceType.contains("l"))
+						{
+							System.out.println("Light bishop placed at: " + position);
+						}
+						else if(pieceType.contains("d"))
+						{
+							System.out.println("Dark bishop placed at: " + position);
+						}
+					}
+					//King
+					else if(pieceType.contains("K"))
+					{
+						if(pieceType.contains("l"))
+						{
+							System.out.println("Light king placed at: " + position);
+						}
+						else if(pieceType.contains("d"))
+						{
+							System.out.println("Dark king placed at: " + position);
+						}
+					}
+					//Knight
+					else if(pieceType.contains("N"))
+					{
+						if(pieceType.contains("l"))
+						{
+							System.out.println("Light knight placed at: " + position);
+						}
+						else if(pieceType.contains("d"))
+						{
+							System.out.println("Dark knight(lol) placed at: " + position);
+						}
+					}
+					//Queen
+					else if(pieceType.contains("Q"))
+					{
+						if(pieceType.contains("l"))
+						{
+							System.out.println("Light queen placed at: " + position);
+						}
+						else if(pieceType.contains("d"))
+						{
+							System.out.println("Dark queen placed at: " + position);
+						}
+					}
+					//Rook
+					else if(pieceType.contains("R"))
+					{
+						if(pieceType.contains("l"))
+						{
+							System.out.println("Light rook placed at: " + position);
+						}
+						else if(pieceType.contains("d"))
+						{
+							System.out.println("Dark rook placed at: " + position);
+						}
+					}
+					//Pawn
+					else if(pieceType.contains("P"))
+					{
+						if(pieceType.contains("l"))
+						{
+							System.out.println("Light pawn placed at: " + position);
+						}
+						else if(pieceType.contains("d"))
+						{
+							System.out.println("Dark pawn placed at: " + position);
+						}
+					}
 				}
 			}
 			System.out.println("");

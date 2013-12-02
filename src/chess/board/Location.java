d5a49ea15ba19edf77d423f18f30269707ccf912
MrExpendable <christianr465@gmail.com>
2013-12-01 20:08:49 -0700

    Fleshed out Location

--- src/chess/board/Location.java
+++ src/chess/board/Location.java
@@ -2,5 +2,27 @@ package chess.board;
 
 public class Location
 {
-
+	private int row, col;
+	
+	public Location(int r, int c)
+	{
+		row = r;
+		col = c;
+	}
+	
+	public int getRow()
+	{
+		return row;
+	}
+	
+	public int getColumn()
+	{
+		return col;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return row + ", " + col;
+	}
 }
